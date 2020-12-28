042da2df0529b21d8c50119c4b334d360858dc06
simone984 <76691200+simone984@users.noreply.github.com>
2020-12-28 14:15:45 +0100

    Add files via upload

---
+++ Add, Commit e Push.py
@@ -0,0 +1,9 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ~/gitTest$ git status
+Sul branch master
+Your branch is up-to-date with 'origin/master'.
+Untracked files:
+  (use "git add ..." to include in what will be committed)
+         <div style="color: red;">index.html</div>
+nothing added to commit but untracked files present (use "git add" to track)
